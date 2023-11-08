5fde74e218667b7bd57dc8bbf55a6943749ea130
Marco <marcofiorito1@gmail.com>
2023-11-08 16:05:36 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
 
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pac-2';
+export class AppComponent implements OnInit{
+  title:any = 'pac-2';
+  posts: any[] = [];
+  comments: any = {};
+  showComments: { [postId: number]: any } = {};
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchPosts();
+  }
+
+  fetchPosts() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(
+      (data: any) => {
+        this.posts = data;
+      },
+      (error) => {
+        console.error('Failed to fetch posts', error);
+      }
+    );
+  }
+
+  toggleComments(postId: number) {
+    if (this.showComments[postId]) {
+      this.showComments[postId] = false;
+    } else {
+      this.fetchComments(postId);
+      this.showComments[postId] = true;
+    }
+  }
+
+  fetchComments(postId: number) {
+    this.http
+      .get(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+      .subscribe(
+        (data: any) => {
+          this.comments[postId] = data;
+        },
+        (error) => {
+          console.error(`Failed to fetch comments for post ${postId}`, error);
+        }
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -10,9 +24,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NzListModule,
+    NzButtonModule,
+    NzCollapseModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [NzMessageService, NzNotificationService, { provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
